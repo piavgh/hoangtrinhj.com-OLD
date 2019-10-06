73f3e6582567aa8307e785fcd7ddcc0c208b09e3
Hoang Trinh <hoang.trinhj@gmail.com>
2019-10-06 22:16:36 +0700

    Update Site Config

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,22 +1,23 @@
 const config = {
-  siteTitle: 'Gatsby Advanced Starter', // Site title.
-  siteTitleShort: 'GA Starter', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: 'GatsbyJS Advanced Starter', // Alternative site title for SEO.
+  siteTitle: 'Hoang Trinh - Developer & Writer', // Site title.
+  siteTitleShort: 'Hoang Trinh Blog', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: 'Hoang Trinh - Developer & Writer', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://vagr9k.github.io', // Domain of your website without pathPrefix.
-  pathPrefix: '/gatsby-advanced-starter', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: 'A GatsbyJS stater with Advanced design in mind.', // Website description used for RSS feeds/meta description tag.
+  siteUrl: 'https://hoangtrinhj.com', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription:
+    'This is my blog, I write about many things include tech, startup, philosophy, ...', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
   siteFBAppID: '1825356251115265', // FB Application ID for using app insights
-  googleAnalyticsID: 'UA-47311644-5', // GA tracking ID.
-  disqusShortname: 'https-vagr9k-github-io-gatsby-advanced-starter', // Disqus shortname.
+  googleAnalyticsID: 'UA-103046645-5', // GA tracking ID.
+  disqusShortname: 'https-hoangtrinhj-com', // Disqus shortname.
   postDefaultCategoryID: 'Tech', // Default category for posts.
   dateFromFormat: 'YYYY-MM-DD', // Date format used in the frontmatter.
   dateFormat: 'DD/MM/YYYY', // Date format for display.
-  userName: 'Advanced User', // Username to display in the author segment.
-  userEmail: 'AdvancedUser@example.com', // Email used for RSS feed's author segment
+  userName: 'Hoang Trinh', // Username to display in the author segment.
+  userEmail: 'hoang.trinhj@gmail.com', // Email used for RSS feed's author segment
   userTwitter: '', // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: 'North Pole, Earth', // User location to display in the author segment.
+  userLocation: 'Ha Noi, Viet Nam', // User location to display in the author segment.
   userAvatar: 'https://api.adorable.io/avatars/150/test.png', // User avatar to display in the author segment.
   userDescription:
     "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
@@ -29,16 +30,16 @@ const config = {
     },
     {
       label: 'Twitter',
-      url: 'https://twitter.com/Vagr9K',
+      url: 'https://twitter.com/hoangtrinhj',
       iconClassName: 'fa fa-twitter',
     },
     {
       label: 'Email',
-      url: 'mailto:vagr9k@gmail.com',
+      url: 'mailto:hoang.trinhj@gmail.com',
       iconClassName: 'fa fa-envelope',
     },
   ],
-  copyright: 'Copyright © 2019. Advanced User', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'Copyright © 2019. hoangtrinhj.com', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#c62828', // Used for setting manifest and progress theme colors.
   backgroundColor: '#e0e0e0', // Used for setting manifest background color.
 }
