6ad8c6e5ccacb99b6250877fb03f972bbc8c65d9
Hoang Trinh <hoang.trinhj@gmail.com>
2019-12-11 16:16:43 +0700

    Fix CSS

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,9 +1,17 @@
 import styled from '@emotion/styled'
 
+import { $mobile } from '../../styles/Variables'
+
 const MainContent = styled.div`
-  margin-top: 92px;
-  min-height: calc(100vh - 219px);
-  padding: 60px 0 0;
+  margin-top: 55px;
+  padding: 40px 0 0;
+  min-height: calc(100vh - 162px);
+
+  @media (min-width: ${$mobile}) {
+    margin-top: 92px;
+    min-height: calc(100vh - 219px);
+    padding: 60px 0 0;
+  }
 `
 
 export default MainContent
