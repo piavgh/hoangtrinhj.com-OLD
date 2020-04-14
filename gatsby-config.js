8668977a1ceb5e794c5e26b3203eec976c4487ce
Hoang Trinh <hoang.trinhj@gmail.com>
2020-04-14 09:15:53 +0700

    Add Support For .mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,12 @@ module.exports = {
             },
           },
           {
+            resolve: `gatsby-remark-autolink-headers`, // this plugin should come before prismjs as described by the docs for this plugin
+            options: {
+              icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-hash"><line x1="4" y1="9" x2="20" y2="9"></line><line x1="4" y1="15" x2="20" y2="15"></line><line x1="10" y1="3" x2="8" y2="21"></line><line x1="16" y1="3" x2="14" y2="21"></line></svg>`,
+            },
+          },
+          {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
               wrapperStyle: `margin-bottom: 1.0725rem`,
@@ -64,15 +70,75 @@ module.exports = {
             resolve: `gatsby-remark-prismjs`,
           },
           {
-            resolve: `gatsby-remark-prismjs`,
-          },
-
-          {
             resolve: `gatsby-remark-copy-linked-files`,
           },
           {
             resolve: `gatsby-remark-smartypants`,
           },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: ['.mdx'],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              quality: 100,
+              maxWidth: 800,
+              backgroundColor: `none`,
+              withWebp: true,
+            },
+          },
+          `gatsby-remark-copy-linked-files`,
+          {
+            resolve: `gatsby-remark-autolink-headers`, // this plugin should come before prismjs as described by the docs for this plugin
+            options: {
+              icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-hash"><line x1="4" y1="9" x2="20" y2="9"></line><line x1="4" y1="15" x2="20" y2="15"></line><line x1="10" y1="3" x2="8" y2="21"></line><line x1="16" y1="3" x2="14" y2="21"></line></svg>`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: `language-`,
+              noInlineHighlight: true,
+              aliases: {},
+            },
+          },
+          `gatsby-remark-embedder`,
+          // TODO: It would be awesome to get this plugin to work so I don't have to manually
+          // run ffmpeg locally for my videos
+          // {
+          //     resolve: `gatsby-remark-videos`,
+          //     options: {
+          //         pipelines: [
+          //             {
+          //                 name: `vp9`,
+          //                 transcode: chain =>
+          //                     chain
+          //                         .videoCodec('libvpx-vp9')
+          //                         .noAudio()
+          //                         .outputOptions(['-crf 20', '-b:v 0']),
+          //                 maxHeight: 720,
+          //                 maxWidth: 1000,
+          //                 fileExtension: `webm`,
+          //             },
+          //             {
+          //                 name: `h264`,
+          //                 transcode: chain =>
+          //                     chain
+          //                         .videoCodec('libx264')
+          //                         .noAudio()
+          //                         .videoBitrate('1000k'),
+          //                 maxHeight: 720,
+          //                 maxWidth: 1000,
+          //                 fileExtension: `mp4`,
+          //             },
+          //         ],
+          //     },
+          // },
         ],
       },
     },
